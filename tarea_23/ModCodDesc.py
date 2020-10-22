0c74a9cd628b60d4e68a22d7e2d1c98d9bf1d082
dobleuvegarcia <67915502+dobleuvegarcia@users.noreply.github.com>
2020-10-22 16:55:50 +0200

    Add files via upload

--- tarea_23/ModCodDesc.py
+++ tarea_23/ModCodDesc.py
@@ -6,7 +6,7 @@ class Jugada():
 	def __init__(self):
 		self.__abc=[]
 		self.__abc=["A","B","C","D","E","F","G","H","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-		self.__palo = ("T","D","C","P")
+		self.__palo = ("Treboles","Diamantes","Corazones","Picas")
 		self.__num = (1,2,3,4,5,6,7,8,9,0,11,12,13)
 		self.__cartas=[]
 		for p in range(len(self.__palo)):
@@ -64,6 +64,15 @@ class Jugada():
 					barajados.insert(new_index, itm)
 					#print(old_index, itm, new_index)
 					break;
+				elif old_index==52:
+					new_index=1
+					#print(new_index)
+					itm=barajados[j_a]
+					barajados.remove(itm)
+					barajados.insert(new_index, itm)
+					#print(old_index, itm, new_index)
+					break;
+
 				else:
 					new_index=old_index+2
 					#print(new_index)
@@ -74,15 +83,70 @@ class Jugada():
 					break;
 		return(barajados)
 
-	def intercambiar(self,pos_jokers):
-		pass
+	def Intercambiar(self,pos_jokers):
+		corte1=[]
+		corte2=[]
+		corte3=[]
+		
+		for j in range(53):
+			if pos_jokers[j][1]!=53:
+				carta=pos_jokers[j]
+				corte1.append(carta)
+			else:
+				i=pos_jokers.index(pos_jokers[j])
+				corte2.insert(0,pos_jokers[j])
+				#print(i)
+				x=i+1
+				break;
+		#print(corte1)
+		for i in range(x,53):
+			if pos_jokers[i][1]!=53:
+				carta=pos_jokers[i]
+				corte2.append(carta)
+			else:
+				x=pos_jokers.index(pos_jokers[i])
+				corte2.insert(x,pos_jokers[i])
+				x=x+1
+				break;
+		#print(corte2)
+		for o in range(x,53):
+			if pos_jokers[o][1]!=53:
+				carta=pos_jokers[o]
+				corte3.append(carta)
+			else:
+				break;
+		#print(corte3)
+		intercambiadas=[]
+		for inter in range(len(corte3)):
+			intercambiadas.append(corte3[inter])
+		for inter in range(len(corte2)):
+			intercambiadas.append(corte2[inter])
+		for inter in range(len(corte1)):
+			intercambiadas.append(corte1[inter])
+		return(intercambiadas)
+	def Codificando(self,intercambiado):
+		ultima_carta=intercambiado[-1]
+		for i in range(len(ultima_carta)):
+			if ultima_carta[i]=="Treboles":
+				suma=ultima_carta[1]
+			elif ultima_carta[i]=="Diamantes":
+				suma=ultima_carta[1]+13
+			elif ultima_carta[i]=="Corazones":
+				suma=ultima_carta[1]+26
+			elif ultima_carta[i]=="Picas":
+				suma=ultima_carta[1]+39
+			elif ultima_carta[i]=="JOKER_A":
+				suma=53
+			elif ultima_carta[i]=="JOKER_B":
+				suma=53
+		
 
 jugar=Jugada()
-
 barajados=jugar.barajar(True)
 baraja2=barajados
 posicionamiento_jokers=jugar.JOKER_A_JOKER_B(barajados)
-intercambiado=intercambiar(posicionamiento_jokers)
+intercambiado=jugar.Intercambiar(posicionamiento_jokers)
+codificar=jugar.Codificando(intercambiado)
 
 
 

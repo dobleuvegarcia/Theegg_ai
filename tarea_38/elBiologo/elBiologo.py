e865e0b974facc061c252ecc5d7bdb6053a355f3
dobleuvegarcia <67915502+dobleuvegarcia@users.noreply.github.com>
2020-11-21 06:10:50 +0100

    Add files via upload

--- tarea_38/elBiologo/elBiologo.py
+++ tarea_38/elBiologo/elBiologo.py
@@ -51,50 +51,48 @@ class Nucleotidos():
 		cont=2
 		for c in comunes:#por cada nucleotido en comun:
 			#print(c)
-			l_ady1+=adn1.count(c)#long de comunes en adn1
-			l_ady2+=adn2.count(c)#long de comunes en adn2
+			l_ady1+=adn1.count(c)#num por nucleotidos comunes en adn1
+			l_ady2+=adn2.count(c)#num por nucleotidos comunes en adn2
 			l_ady=min(l_ady1,l_ady2)#long max de secuencia=minima en comun
 			ady_l+=c#cadena letras de nucleotidos
-			ady=0#guardaremos la longitud maxima del patron en cadenas de adn
+			ady=0#inicialiar la longitud maxima del patron en cadenas de adn
 			for l in range(l_ady):#en el rango de secuencia maxima en comun
 			#hallamos las posibles combinaciones de nucleotidos (ady_l)
 			#desde cont=2 parejas hasta que cont=l_ady (rango de secuencia maxima en comun)
 				pos_comb_ady=list(itertools.product(ady_l, repeat=cont))
-				#recorremos las posibles combinaciones 
-				#de adyacentes lista comun de nucleotidos(comunes)
-				for p in range(len(pos_comb_ady)):
-					patron=""#inicializamos nomenclaturas, nucleotidos en comun
-					for p1 in pos_comb_ady[p]:
-						patron+=p1#añadimos la
-						#print(patron)
-						#buscamos el patron en adn1 de las posibles combinaciones
-						a1=re.findall(patron, adn1)
-						#buscamos el patron en adn2 de las posibles combinaciones
-						a2=re.findall(patron, adn2)
-						#recorremos ese patron para saber si es comun en los adns
-						for n in a1:
-							for h in a2:
-								if n==h:#si es comun
-									y=len(n)#hallamos la longitud
-									if y>ady:
-										ady=y#guardamos la longitud mas larga
-										adyacentes=n#guardamos la secuencia de longitud maxima
-
-								else: #sino liberamos memoria borrando el patron 
-								#generado en posibles combinaciones
-									pos_comb_ady.remove(n)
-
 			cont+=1
+		#recorremos las posibles combinaciones adyacentes
+		for p in range(len(pos_comb_ady)):
+			patron=""#inicializar/limpiar nomenclaturas, nucleotidos en comun
+			for p1 in pos_comb_ady[p]:#sacamos la primera posible combinacion adyacente
+				patron+=p1#añadimos las numenclaturas para sacar el patron
+				#buscamos el patron en adn1 de las posibles combinaciones
+				a1=re.findall(patron, adn1)
+				#buscamos el patron en adn2 de las posibles combinaciones
+				a2=re.findall(patron, adn2)
+				#extremos el patron de a1(n) y a2(h) encontrados
+				#recorremos para saber si es igual en los adns
+				for n in a1:
+					for h in a2:
+						if n==h:#si los patrones son iguales
+							y=len(n)#hallamos la longitud
+							if y>ady:#comparamos si la longitud es mayor a la encontrada
+								ady=y#guardamos la longitud mas larga
+								adyacentes=n#guardamos la secuencia de longitud maxima
+
+						else: #sino liberamos memoria borrando el patron 
+						#generado en posibles combinaciones
+							pos_comb_ady.remove(n)
+
 
 			"""solo visualizar por pantalla, para correccion.
 			 muestra cadena de nucleotidos en comun con los 2 ADNs. 
 			 podria haber nucleotidos que no existen en alguna
 			 cadena de ADN y son excluidos de posibles combinaciones._______
 
-			 _________________________________________________________
+			 ______________________________________________________________"""
 
-
-		print("buscar: NUCLEOTIDOS COMUNES en 2 ADNs :  ",ady_l)_____"""
+		print("buscar: NUCLEOTIDOS COMUNES en 2 ADNs :  ",ady_l)
 		return(adyacentes)
 
 nu=Nucleotidos()
@@ -109,9 +107,9 @@ POR PANTALLA.
 EN ESTE EJERCICIO SOLO SE PIDE EJECUCION NI ENTRADA NI DALIDA DE DATOS:
  desactivar las comillas SOLO para LA DEMOSTRACION 
  Y VISUALIZACION DE PANTALLA DE RESULTADOS:
- ____________________________________________________
+ ___________________________________________________________"""
 
 print("1ª ADN ALEATORIO:  ",adn1)
 print("2ª ADN ALEATORIO:  ",adn2)
-print("MAXIMOS NUCLEOTIDOS COMUNES ADNs:" ,ady)_______"""
+print("MAXIMOS NUCLEOTIDOS COMUNES ADNs:" ,ady)
 

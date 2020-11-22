e73f2e216da2266a43f809c0f2f0316104d179fc
dobleuvegarcia <67915502+dobleuvegarcia@users.noreply.github.com>
2020-11-22 21:48:02 +0100

    Add files via upload

--- tarea_38/invertirPalabras/invertirPalabras.py
+++ tarea_38/invertirPalabras/invertirPalabras.py
@@ -8,9 +8,9 @@ class InvertirParametrosDeValores():
 	 		self.params_valor.append(f)#añadimos los parametros por cada valor
 
 	def InvertirParametros(self):
-		params_invertir=[]
+		params_invertir=[]#lista de valores con lista parametros invertidos
 		for n in range(len(self.params_valor)):#en el rango de todos las valores introducidos
-			conj_params=[]
+			conj_params=[]#lista de conjunto de parametros por valor
 			parametro=""
 			f_arch=len(self.params_valor[n])-1#variable fin_archivo de los parametros por valor
 			for p in  range(len(self.params_valor[n])):#recorremos los parametros de cada valor
@@ -24,18 +24,20 @@ class InvertirParametrosDeValores():
 		return(params_invertir)#retornamos conjunto de valores con los parametros invertidas
 
 	def Mostrar(self,f):
-		#en este metodo recoremos los parametros invertidos de un valor 
+		#en este metodo recorremos de lista de  parametros invertidos por cada lista de un valor f
 		self.f=f
 		parametro=""
-		for i in range(len(self.f)):
-			parametro+=self.f[i]+" "
-		return(parametro)#devolvemos el valor con las parametro invertidas
+		#recoremos la lista de parametros(f) para crear la cadena a mostrar separados por espacio en blanco
+		for i in range(len(self.f)):#recorremos i parametros por valor long de cada valor
+			parametro+=self.f[i]+" "#guardamos cada parametro en una cadena separada por espacio en blanco
+		return(parametro)#devolvemos la cadena de parametros de ese valor f
 
 
 
 arr_frases=InvertirParametrosDeValores()
 invertir=arr_frases.InvertirParametros()
-for i in range(len(invertir)):#en el rango de todos los valores
+for i in range(len(invertir)):#en el rango de la lista valores invertidos
 	f=invertir[i]#por cada valor invertido
-	pantalla=arr_frases.Mostrar(f)#mostramos en pantalla el contenido del valor(i)
+	pantalla=arr_frases.Mostrar(f)#la llamada Mostrar() nos devuelve por cada valor f
+	#una cadena de parametros separados por un espacio en blanco
 	print("CASE #", i+1,": ", pantalla)

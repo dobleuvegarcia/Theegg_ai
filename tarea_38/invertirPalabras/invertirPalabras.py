3ddad664fedad6605ebc0521254b50bfbac3ae60
dobleuvegarcia <67915502+dobleuvegarcia@users.noreply.github.com>
2020-11-22 21:15:49 +0100

    Add files via upload

--- tarea_38/invertirPalabras/invertirPalabras.py
+++ tarea_38/invertirPalabras/invertirPalabras.py
@@ -1,43 +1,41 @@
-class InvertirPalabras():
+class InvertirParametrosDeValores():
 
-	def PedirDatos(self):
-		self.num=int(input("introduce un numero de casos a invertir:  "))
-		self.frases=[]
-		for i in range(self.num):
-	 		f=input("introduce frase a invertir:  ")
-	 		self.frases.append(f)
-		return(self.frases)
+	def __init__(self):
+		self.valores=int(input("Cantidad de valores a analizar :  "))
+		self.params_valor=[]#parametros por valor
+		for i in range(self.valores):#por cada rango de valores
+	 		f=input("parametros a invertir, valor N "+(str(i+1))+" :  ")
+	 		self.params_valor.append(f)#añadimos los parametros por cada valor
 
-	def InvertirFrases(self):
-		f_invertir=[]
-		for n in range(len(self.frases)):#en el rango de todas las frases introducidas
-			separador=[]
-			n_frase=self.frases[n]
-			palabra=""
-			terminado=len(n_frase)-1
-			for p in  range(len(n_frase)):#recorremos la frase
-				palabra+=n_frase[p] #guardamos la palabras
-				if n_frase[p]==" " or p==terminado: #separadas por espacios o hasta que la frase termina
-					separador.append(palabra)#añadimos las palabras de una frase
-					palabra=""
-			separador.reverse()#invertimos la frase
-			f_invertir.append(separador)#añadimos frases invertidas
-		return(f_invertir)#retornamos conjunto frases invertidas
+	def InvertirParametros(self):
+		params_invertir=[]
+		for n in range(len(self.params_valor)):#en el rango de todos las valores introducidos
+			conj_params=[]
+			parametro=""
+			f_arch=len(self.params_valor[n])-1#variable fin_archivo de los parametros por valor
+			for p in  range(len(self.params_valor[n])):#recorremos los parametros de cada valor
+				parametro+=self.params_valor[n][p] #guardamos por el contenido del parametro 
+				#hasta encontrar un espacio en blanco o fin de parametros por valor(fin_arch)
+				if self.params_valor[n][p]==" " or p==f_arch: #separadas por espacios o hasta fin del valor
+					conj_params.append(parametro)#añadimos las parametro de cada valor
+					parametro=""#limpiamos contenido parametro
+			conj_params.reverse()#invertimos los parametros del valor(n) 
+			params_invertir.append(conj_params)#añadimos los parametros invertidos por cada valor
+		return(params_invertir)#retornamos conjunto de valores con los parametros invertidas
 
 	def Mostrar(self,f):
-		#en este metodo recoremos las palabras invertidas de una frase 
+		#en este metodo recoremos los parametros invertidos de un valor 
 		self.f=f
-		palabras=""
+		parametro=""
 		for i in range(len(self.f)):
-			palabras+=self.f[i]+" "
-		return(palabras)#devolvemos la frase con las palabras invertidas
+			parametro+=self.f[i]+" "
+		return(parametro)#devolvemos el valor con las parametro invertidas
 
 
 
-arr_frases=InvertirPalabras()
-arr_frases.PedirDatos()
-invertir=arr_frases.InvertirFrases()
-for i in range(len(invertir)):#por cada frase invertida
-	f=invertir[i]
-	pantalla=arr_frases.Mostrar(f)#mostramos en pantalla la frase de i
-	print("CASE #", i,": ", pantalla)
+arr_frases=InvertirParametrosDeValores()
+invertir=arr_frases.InvertirParametros()
+for i in range(len(invertir)):#en el rango de todos los valores
+	f=invertir[i]#por cada valor invertido
+	pantalla=arr_frases.Mostrar(f)#mostramos en pantalla el contenido del valor(i)
+	print("CASE #", i+1,": ", pantalla)

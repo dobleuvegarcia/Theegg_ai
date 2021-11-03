c3f5171f1ade7e4c495c5e7c03932853d65346ba
dobleuvegarcia <67915502+dobleuvegarcia@users.noreply.github.com>
2021-11-04 00:34:33 +0100

    Add files via upload

--- tarea_26/tarea_26_1.py
+++ tarea_26/tarea_26_1.py
@@ -6,14 +6,10 @@ import statistics as stat
 data=pd.read_csv('26_1.csv',encoding="ISO-8859-1", header=0, sep = ';')
 data_copy=data.copy
 registro=len(data)
-print(registro)
-print(data.head(registro))
-print(data.describe())
-print(pd.to_datetime(data["Baliozkotze-data / Fecha Validacion"],dayfirst='True').dt.strftime("%y%m%d"))
+data.head(registro)
+data.describe()
+pd.to_datetime(data["Baliozkotze-data / Fecha Validacion"],dayfirst='True').dt.strftime("%y%m%d")
 dias=(data["Baliozkotze-data / Fecha Validacion"].astype('datetime64[D]').dt.dayofyear)
-#plt.rcParams["figure.figsize"]=(16,12)
-#grafico=sn.countplot(x=data["Baliozkotze-data / Fecha Validacion"],data=data)
-#ax=data.plot.hist(by='Baliozkotze-data / Fecha Validacion',bins=12, alpha=0.5,range=(1,365))
 x=data["Baliozkotze-data / Fecha Validacion"]
 y=data["Araba: Positiboa / Álava: Positivo "]
 x = np.arange(0, registro)
@@ -24,7 +20,14 @@ x1 = np.arange(0, registro)
 plt.plot(x1, y1,label='Bizkaia: Positivo')
 plt.xlabel('dias')
 plt.ylabel('nº contagios')
-plt.title('nº de contagios por '+str(registro)+' dias(01/03/2020 hasta 12/05/2020)\n'+'CAE Media:'+str(np.mean(data["EAE: Positiboa / CAE: Positivo "]))+"\n"+'CAE Moda: '+str(stat.mode(data["EAE: Positiboa / CAE: Positivo "]))+"\n"+'CAE Mediana: '+str(stat.median(data["EAE: Positiboa / CAE: Positivo "])),fontsize = 7,color='red')
+plt.title('nº de contagios por '+str(registro)+' dias(01/03/2020 hasta 12/05/2020)\n'
+	+'CAE Media:'+str(np.mean(data["EAE: Positiboa / CAE: Positivo "]))+"  "
+	+'CAE Moda: '+str(stat.mode(data["EAE: Positiboa / CAE: Positivo "]))+"  "
+	+'CAE Mediana: '+str(stat.median(data["EAE: Positiboa / CAE: Positivo "]))+"\n"
+	+'CAE Varianza:'+str(np.var(data["EAE: Positiboa / CAE: Positivo "]))+"  "
+	+'CAE Desviacion Estandar: '+str(np.std(data["EAE: Positiboa / CAE: Positivo "]))+"  "
+	+'CAE Rango: '+str(np.max(data["EAE: Positiboa / CAE: Positivo "])-np.min(data["EAE: Positiboa / CAE: Positivo "]))+"\n"
+		,fontsize = 7)
 x2=data["Baliozkotze-data / Fecha Validacion"]
 y2=data["Gipuzkoa: Positiboa / Gipuzkoa: Positivo "]
 x2 = np.arange(0, registro)

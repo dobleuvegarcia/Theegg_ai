a01e817e3e2e10fdcc2b7096e3cb9c196a8b1ac7
dobleuvegarcia <67915502+dobleuvegarcia@users.noreply.github.com>
2021-11-06 20:52:48 +0100

    Add files via upload

--- tarea_26/tarea_26_1.py
+++ tarea_26/tarea_26_1.py
@@ -10,35 +10,41 @@ data.head(registro)
 data.describe()
 pd.to_datetime(data["Baliozkotze-data / Fecha Validacion"],dayfirst='True').dt.strftime("%y%m%d")
 dias=(data["Baliozkotze-data / Fecha Validacion"].astype('datetime64[D]').dt.dayofyear)
-x=data["Baliozkotze-data / Fecha Validacion"]
+x=dias
 y=data["Araba: Positiboa / Álava: Positivo "]
 x = np.arange(0, registro)
 plt.plot(x, y,label='Álava: Positivo')
-x1=data["Baliozkotze-data / Fecha Validacion"]
+x1=dias
 y1=data["Bizkaia: Positiboa / Bizkaia: Positivo "]
 x1 = np.arange(0, registro)
 plt.plot(x1, y1,label='Bizkaia: Positivo')
-plt.xlabel('dias')
-plt.ylabel('nº contagios')
-plt.title('nº de contagios por '+str(registro)+' dias(01/03/2020 hasta 12/05/2020)\n'
-	+'CAE Media:'+str(np.mean(data["EAE: Positiboa / CAE: Positivo "]))+"  "
-	+'CAE Moda: '+str(stat.mode(data["EAE: Positiboa / CAE: Positivo "]))+"  "
-	+'CAE Mediana: '+str(stat.median(data["EAE: Positiboa / CAE: Positivo "]))+"\n"
-	+'CAE Varianza:'+str(np.var(data["EAE: Positiboa / CAE: Positivo "]))+"  "
-	+'CAE Desviacion Estandar: '+str(np.std(data["EAE: Positiboa / CAE: Positivo "]))+"  "
-	+'CAE Rango: '+str(np.max(data["EAE: Positiboa / CAE: Positivo "])-np.min(data["EAE: Positiboa / CAE: Positivo "]))+"\n"
-		,fontsize = 7)
-x2=data["Baliozkotze-data / Fecha Validacion"]
+x2=dias
 y2=data["Gipuzkoa: Positiboa / Gipuzkoa: Positivo "]
 x2 = np.arange(0, registro)
 plt.plot(x2, y2,label='Gipuzkoa: Positivo')
-
-xt=data["Baliozkotze-data / Fecha Validacion"]
+xt=dias
 yt=data["EAE: Positiboa / CAE: Positivo "]
 xt = np.arange(0, registro)
 plt.plot(xt, yt,label='CAE: Positivo')
+plt.xlabel('dias')
+plt.ylabel('nº contagios')
+
+media=np.mean(data["EAE: Positiboa / CAE: Positivo "])
+moda=stat.mode(data["EAE: Positiboa / CAE: Positivo "])
+mediana=stat.median(data["EAE: Positiboa / CAE: Positivo "])
+varianza=np.var(data["EAE: Positiboa / CAE: Positivo "])
+desviacion_estandar=np.std(data["EAE: Positiboa / CAE: Positivo "])
+rango=np.max(data["EAE: Positiboa / CAE: Positivo "]-np.min(data["EAE: Positiboa / CAE: Positivo "]))
+plt.title('nº de contagios por '+str(registro)+' dias(01/03/2020 hasta 12/05/2020)\n'
+	+'CAE Media:'+str(media)+"  "
+	+'CAE Moda: '+str(moda)+"  "
+	+'CAE Mediana: '+str(mediana)+"\n"
+	+'CAE Varianza:'+str(varianza)+"  "
+	+'CAE Desviacion Estandar: '+str(desviacion_estandar)+"  "
+	+'CAE Rango: '+str(rango)+"\n"
+		,fontsize = 7)
+
 
 plt.legend()
 plt.savefig('comunidades_autonomas_covid.png')
 plt.show()
-

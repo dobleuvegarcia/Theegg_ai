e8eb8a1a7036cc8b93a892a4c0a7e46f1d103972
dobleuvegarcia <67915502+dobleuvegarcia@users.noreply.github.com>
2022-01-03 18:21:27 +0100

    Add files via upload

---
+++ vanesa/tarea_95/95_01/comparacion_dos_numeros.py
@@ -0,0 +1,11 @@
+import math
+
+def comparacion_numeros(num1, num2):
+	if num1==num2:
+		print("Estos numeros son iguales")
+	elif num1!=num2:
+		print("Estos numeros son diferentes")
+	elif num1>num2:
+		print(str(num1)+" es mayor que "+str(num2))
+	elif num2>=num1:
+		print("El "+str(num2)+" es mayor o igual al numero "+str(num1))
---
+++ vanesa/tarea_95/95_01/index_comparacion_dos_numeros .py
@@ -0,0 +1,4 @@
+import comparacion_dos_numeros as cdn
+num1=input("Introduce un numero:  ")
+num2=input("Introduce un segundo numero:  ")
+cdn.comparacion_numeros(num1, num2)

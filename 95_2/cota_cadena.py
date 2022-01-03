5df1c980cc2d7c5942273353106264f55ef24bb5
dobleuvegarcia <67915502+dobleuvegarcia@users.noreply.github.com>
2022-01-03 22:33:57 +0100

    Delete 95_2 directory

--- 95_2/cota_cadena.py
+++
@@ -1,8 +0,0 @@
-
-def pedir_cadena_texto():
-	text=input("introduce una cadena de texto: ")
-	return(text)
-def cota_texto(text):
-	longitud=len(text)
-	if longitud>=5 and longitud<10:
-		print("el numero introducido es >=5 y < 10")

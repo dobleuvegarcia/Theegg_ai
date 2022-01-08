678bc3dc43e262b0a0c8d638309d8574182236bb
dobleuvegarcia <67915502+dobleuvegarcia@users.noreply.github.com>
2022-01-08 15:51:01 +0100

    Add files via upload

--- tarea_95/95_2/cota_cadena.py
+++ tarea_95/95_2/cota_cadena.py
@@ -5,4 +5,6 @@ def pedir_cadena_texto():
 def cota_texto(text):
 	longitud=len(text)
 	if longitud>=5 and longitud<10:
-		print("el numero introducido es >=5 y < 10")
+		print("El espacio de la cadena de texto introducida es  >=5 y < 10")
+	else:
+		print("El espacio de la cadena de texto introducida es <5 ó > 10")
